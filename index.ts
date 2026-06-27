bccb0c00a2d350d48fc46451468be0a777b813a8
Drew Gingerich <drew@elsewhere.space>
2026-06-26 22:43:03 -0700

    Create presentation

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
+declare module "*.css";
+
 import "reveal.js/reveal.css";
 import "reveal.js/theme/black.css";
+import "./index.css";
 
 import Reveal from "reveal.js";
 
-Reveal.initialize();
+Reveal.initialize({
+  transitionSpeed: "fast",
+});
